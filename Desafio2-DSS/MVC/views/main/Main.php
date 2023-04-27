dd271c93ec8afa30608b9dd2ccf96b82d3c57716
l1teeee <89312593+l1teeee@users.noreply.github.com>
2023-04-27 16:15:25 -0600

    Update

--- Desafio2-DSS/MVC/views/main/Main.php
+++ Desafio2-DSS/MVC/views/main/Main.php
@@ -14,6 +14,13 @@
         
         <title>Despacho y Venta de Gasolina</title>
     </head>
+    <nav class="navbar bg-body-tertiary">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+      <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Bootstrap" width="30" height="24">
+    </a>
+  </div>
+</nav>
     <body></body>
         <br><br>
         <h1 class="text-center">Gasolinera los Empresaurios</h1>
